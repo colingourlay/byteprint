10ab88e35941352163deb34a4b65f073577a556d
Colin Gourlay <colin.j.gourlay@gmail.com>
2009-12-31 03:53:18 +1000

    Completed admin menu functionality (progressively enhanced), and credited icon authors

--- column/content/platform/admin/js/collapsible_menu.js
+++ column/content/platform/admin/js/collapsible_menu.js
@@ -1,26 +1,17 @@
 $(document).ready(function(){
-    $('ul.menu ul').hide();
-    $.each($('ul.menu'), function(){
-        $('#' + this.id + '.expandfirst ul:first').show();
-    });
-    $('ul.menu li a').click(function() {
-        var checkElement = $(this).next();
-        var parent = this.parentNode.parentNode.id;
-
-        if($('#' + parent).hasClass('noaccordion')) {
-            $(this).next().slideToggle('normal');
-            return false;
-        }
-        if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-            if($('#' + parent).hasClass('collapsible')) {
-                $('#' + parent + ' ul:visible').slideUp('normal');
-            }
-            return false;
-        }
-        if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-            $('#' + parent + ' ul:visible').slideUp('normal');
-            checkElement.slideDown('normal');
-            return false;
+    $('dd').hide()
+    $('dd ul li.current').parent().parent().show()
+    $('dt').addClass('closed');
+    $('dd ul li.current').parent().parent().prev().removeClass('closed');
+    $('dt .toggle').show();
+    $('dt .toggle').click(function(){
+        if ($(this).parent().hasClass('closed')) {
+            $(this).parent().removeClass('closed');
+            $(this).parent().next().slideDown('fast');
+        } else {
+            $(this).parent().next().slideUp('fast', function() {
+                $(this).prev().addClass('closed');
+            });
         }
     });
 });
