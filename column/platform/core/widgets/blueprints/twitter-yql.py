81de1dfdfde12cefe8bc921eef76eb61290434cb
Colin Gourlay <colin.j.gourlay@gmail.com>
2010-01-22 14:09:22 +1000

    Added client-side Twitter widget.

---
+++ column/platform/core/widgets/blueprints/twitter-yql.py
@@ -0,0 +1,34 @@
+# from django import forms
+# from platform.core.widgets import Blueprint
+# import urllib2
+# from django.utils import simplejson
+# 
+# class TwitterYQLBlueprint(Blueprint):
+# 
+#     name = 'twitter-yql'
+#     display_name = 'Twitter (Server-side YQL)'
+#     preview = True
+#     fields = {
+#         'username': forms.CharField(
+#             required = False,
+#             label = "Twitter User",
+#             help_text = "Enter your Twitter username."
+#         ),
+#         'num_tweets': forms.IntegerField(
+#             required = False,
+#             label = "Number of Tweets",
+#             help_text = "The number of tweets you want to be displayed inside \
+#                 the widget. If you leave this blank, it will default to 1."
+#         )
+#     }
+# 
+#     def render(self, widget_data):
+#         output = ""
+#         if widget_data['username']:
+#             num_tweets = 1
+#             if widget_data['num_tweets']:
+#                 num_tweets = widget_data['num_tweets']
+#         result = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20title%2Cabstract%20from%20search.web%20where%20query%3D%22colin%20gourlay%22&format=json").read()
+#         data = simplejson.loads(result)
+#         output += data['query']['results']['result'][0]['title']
+#         return output
--- column/platform/core/widgets/blueprints/twitter.py
+++ column/platform/core/widgets/blueprints/twitter.py
@@ -1,34 +1,35 @@
-# from django import forms
-# from platform.core.widgets import Blueprint
-# import urllib2
-# from django.utils import simplejson
-# 
-# class YQLTwitterBlueprint(Blueprint):
-# 
-#     name = 'yql-twitter'
-#     display_name = 'YQL Twitter'
-#     preview = True
-#     fields = {
-#         'username': forms.CharField(
-#             required = False,
-#             label = "Twitter User",
-#             help_text = "Enter your Twitter username."
-#         ),
-#         'num_tweets': forms.IntegerField(
-#             required = False,
-#             label = "Number of Tweets",
-#             help_text = "The number of tweets you want to be displayed inside \
-#                 the widget. If you leave this blank, it will default to 1."
-#         )
-#     }
-# 
-#     def render(self, widget_data):
-#         output = ""
-#         if widget_data['username']:
-#             num_tweets = 1
-#             if widget_data['num_tweets']:
-#                 num_tweets = widget_data['num_tweets']
-#         result = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20title%2Cabstract%20from%20search.web%20where%20query%3D%22colin%20gourlay%22&format=json").read()
-#         data = simplejson.loads(result)
-#         output += data['query']['results']['result'][0]['title']
-#         return output
+from django import forms
+from platform.core.widgets import Blueprint
+
+class TwitterBlueprint(Blueprint):
+
+    name = 'twitter'
+    display_name = 'Twitter'
+    preview = True
+    fields = {
+        'username': forms.CharField(
+            required = False,
+            label = "Twitter User",
+            help_text = "Enter the name of the Twitter user who's timeline you \
+                want to display."
+        ),
+        'count': forms.IntegerField(
+            required = False,
+            label = "Number of Tweets",
+            help_text = "Enter the number of tweets you want to be displayed. \
+                If you leave this blank, it will default to 1."
+        )
+    }
+
+    def render(self, widget_data):
+        output = ""
+        if widget_data['username']:
+            count = 1
+            if widget_data['count']:
+                count = widget_data['count']
+                output += "<p><a href=\"http://www.twitter.com/" + widget_data['username'] + "/\">Follow @" + widget_data['username'] + " on Twitter</a></p>"
+            output += "<ul id=\"twitter_update_list\"><li>You need to have javascript enabled to view <a href=\"http://www.twitter.com/" + widget_data['username'] + "/\">@" + widget_data['username'] + "</a>'s Twitter status</li></ul>"
+            output += "<script type=\"text/javascript\" src=\"http://twitter.com/javascripts/blogger.js\"></script>"    
+            output += "<script type=\"text/javascript\" src=\"http://twitter.com/statuses/user_timeline/" + widget_data['username'] + ".json?callback=twitterCallback2&count=" + count + "\"></script>"
+            print output
+        return output
