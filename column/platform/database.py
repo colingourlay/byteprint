b8b1c0c3a053426136e60246414625cb57521e36
Colin Gourlay <colin.j.gourlay@gmail.com>
2010-02-18 10:18:19 +1000

    Updated database settings file documentation, and preloaded the conifguration with a local sqlite database.

--- column/platform/database.py
+++ column/platform/database.py
@@ -1,6 +1,29 @@
+# == DATABASE SETTINGS ======================================================= #
+#                                                                              #
+# If you are using sqlite, only NAME and ENGINE are required. For other local  #
+# databases, you also need to complete the USER & PASSWORD fields. If the      #
+# database is on a separate host, enter the relevant HOST and PORT details.    #
+# OPTIONS is reserved for special commands that may need to be run against the #
+# database. This can be left blank.                                            #
+#                                                                              #
+# 'NAME'     - Database name / path to a local sqlite database                 #
+# 'ENGINE'   - django.db.backends.* that connects to your database             #
+# 'USER'     - Database username                                               #
+# 'PASSWORD' - Database password                                               #
+# 'HOST'     - Database host (defaults to localhost if left blank)             #
+# 'PORT'     - Database port (used with the database host)                     #
+# 'OPTIONS'  - Special options to be interpreted by the database engine        #
+#                                                                              #
+# To get you up and running quickly, settings have already been provided for a #
+# local sqlite database, which will allow you to complete the installation     #
+# process, and have a look at the system. You should change these settings to  #
+# those of your real database before performing a permanent installation.      #
+#                                                                              #
+# ============================================================================ #
+
 DATABASES = {
     'default': {
-        'NAME': '/Users/colin/db/db.sqlite',
+        'NAME': 'platform/testdb.sqlite',
         'ENGINE': 'django.db.backends.sqlite3',
         'USER': '',
         'PASSWORD': '',
@@ -9,24 +32,3 @@ DATABASES = {
         'OPTIONS': {},
     }
 }
-
-# DATABASES = {           #
-#     'default': {        # 
-#         'NAME': '',     # - The name of your database, or a path to sqlite DB
-#         'ENGINE': '',   # - django.db.backends.* that connects to your DB
-#         'USER': '',     # - DB username 
-#         'PASSWORD': '', # - DB password
-#         'HOST': '',     # - DB remote host (defaults to localhost)
-#         'PORT': '',     # - Remote host port for DB access
-#         'OPTIONS': {},  # - Special options to be interpreted by the DB
-#     }                   #
-# }                       #
-#                         # == HELP ============================================
-#                         # If you are using sqlite, only NAME and ENGINE are
-#                         # required. For other local databases, you also need
-#                         # to complete the USER & PASSWORD fields. If the
-#                         # database is on a separate host, enter the relevant
-#                         # HOST and PORT details. OPTIONS is reserved for
-#                         # special commands that may need to be run against the
-#                         # database. This can be left blank.
-#                         # ====================================================
