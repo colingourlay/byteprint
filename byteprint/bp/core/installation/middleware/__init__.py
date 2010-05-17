733b1be176de820d7a5d7d7ca2a598834fd69a2e
Colin Gourlay <colin.j.gourlay@gmail.com>
2010-05-17 23:56:07 +1000

    Added extra check for the favicon in the installation middleware

--- byteprint/bp/core/installation/middleware/__init__.py
+++ byteprint/bp/core/installation/middleware/__init__.py
@@ -1,15 +1,16 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 from bp.core.installation import is_installed
 
 class InstallationMiddleware(object):
 
     def process_request(self, request):
-        if request.path.startswith('/static/'):
+        if request.path.startswith('/static/') or request.path == "/favicon.ico":
             return None
         if request.path == reverse('installation_install'):
             return None
         if is_installed():
             return None
-        return HttpResponseRedirect(reverse('installation_install'))
+        return redirect('installation_install')
